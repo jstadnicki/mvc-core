b7e27bd28133f949bb4f2e690821eb9576202d7b
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2017-01-18 23:48:13 +0100

    displaying static text

--- MvcCoreTodo/src/MvcCoreTodo/Controllers/HomeController.cs
+++ MvcCoreTodo/src/MvcCoreTodo/Controllers/HomeController.cs
@@ -1,35 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-namespace MvcCoreTodo.Controllers
+﻿namespace MvcCoreTodo.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            return this.View("Index");
         }
     }
 }

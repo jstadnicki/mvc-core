9124d20b904645c4cf10c5058b88b8473bcb49d6
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2017-01-19 00:19:20 +0100

    dispaying static todo list

--- MvcCoreTodo/src/MvcCoreTodo/Controllers/HomeController.cs
+++ MvcCoreTodo/src/MvcCoreTodo/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 ﻿namespace MvcCoreTodo.Controllers
 {
+    using System.Collections.Generic;
+
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return this.View("Index");
+            var list = new List<TodoItem>();
+            list.Add(new TodoItem { Text = "Napisać na blogu" });
+            list.Add(new TodoItem { Text = "Czytać przez 15 minut" });
+            list.Add(new TodoItem { Text = "Ogarnąć prezentacje" });
+            list.Add(new TodoItem { Text = "Umyć zęby" });
+
+            return this.View("Index", list);
         }
+    }
+
+    public class TodoItem
+    {
+        public string Text { get; set; }
     }
 }
